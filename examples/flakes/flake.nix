3d99673e391d4a458c3dd8c79938a40e10b98f06
Matthias Debernardini <32387851+matthiasdebernardini@users.noreply.github.com>
2023-05-12 17:01:33 -0500

    Update flake.nix

--- examples/flakes/flake.nix
+++ examples/flakes/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A basic nix-bitcoin node";
 
-  inputs.nix-bitcoin.url = "github:fort-nix/nix-bitcoin/release";
+  inputs.nix-bitcoin.url = "github:matthiasdebernardini/nix-bitcoin/release";
 
   outputs = { self, nix-bitcoin }: {
 
